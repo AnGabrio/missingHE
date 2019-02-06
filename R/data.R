c796081a6d1a61e82acfe444cded3d075a735346
AnGabrio <ucakgab@ucl.ac.uk>
2019-02-06 17:35:18 +0000

    update dataset

--- R/data.R
+++ R/data.R
@@ -2,21 +2,16 @@
 #'
 #' Data from a pilot RCT trial (The MenSS trial) on youn men at risk of Sexually Trasmitted Infections (STIs).
 #' A total of 159 individuals were enrolled in trial: 75 in the control (t=1) and 84 in the active intervention (t=2).
-#' Health related quality of life and cost data were collected via self-reported questionnaires at 4 time points throughout the study: at baseline,
+#' Health related quality of life and cost data were collected via self-reported questionnaires at four time points throughout the study: at baseline,
 #' 3 months, 6 months and 12 months follow-up. QALYs and total costs were then computed by combining the utility scores using the area under the curve method
-#' and by summing up the cost components at each time point. 
+#' and by summing up the cost components at each time point. Baseline data are available for the utilities (no baseline costs collected), 
+#' age, ethnicity and employment variables 
 #'
 #' \describe{
 #'   \item{id}{id number}
-#'   \item{u.0}{baseline utilities}
-#'   \item{u.1}{utilities at 3-months follow-up}
-#'   \item{u.2}{utilities at 6-months follow-up}
-#'   \item{u.3}{utilities at 12-months follow-up}
 #'   \item{e}{Quality Adjusted Life Years (QALYs)}
-#'   \item{c.1}{costs in pounds at 3-months follow-up}
-#'   \item{c.2}{costs in pounds at 6-months follow-up}
-#'   \item{c.3}{costs in pounds at 12-months follow-up}
 #'   \item{c}{Total costs in pounds}
+#'   \item{u.0}{baseline utilities}
 #'   \item{age}{Age in years}
 #'   \item{ethnicity}{binary: white (1) and other (0)}
 #'   \item{employment}{binary: working (1) and other (0)}
@@ -33,7 +28,7 @@
 #'
 #'
 #' @examples
-#' data(MenSS)
+#' MenSS <- data(MenSS)
 #' summary(MenSS)
 #' str(MenSS)
 "MenSS"
