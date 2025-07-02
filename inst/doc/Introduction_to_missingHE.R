345246b9862d39cc5f4e7bb018cfc473ff737036
Andrea Gabrio <30290038+AnGabrio@users.noreply.github.com>
2025-07-02 15:38:28 +0200

    Add files via upload

--- inst/doc/Introduction_to_missingHE.R
+++ inst/doc/Introduction_to_missingHE.R
@@ -1,10 +1,10 @@
-## ---- echo = FALSE, message = FALSE-------------------------------------------
+## ----echo = FALSE, message = FALSE--------------------------------------------------------------------------------
 knitr::opts_chunk$set(prompt = TRUE, highlight = F, background = '#FFFFFF',
                       collapse = T, comment = "#>")
 library(missingHE)
 set.seed(1014)
 
-## ----menss--------------------------------------------------------------------
+## ----menss--------------------------------------------------------------------------------------------------------
 str(MenSS)
 
 ## ----hist, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE, fig.width=15,fig.height=9,out.width='65%',fig.align='center'----
@@ -14,7 +14,7 @@ hist(MenSS$e[MenSS$t==2], main = "QALYs - Intervention")
 hist(MenSS$c[MenSS$t==1], main = "Costs - Control")
 hist(MenSS$c[MenSS$t==2], main = "Costs - Intervention")
 
-## ----sv, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----sv, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE---------------------------------
 #proportions of ones and zeros in the control group
 c(sum(MenSS$e[MenSS$t==1]==1, na.rm = TRUE) / length(MenSS$e[MenSS$t==1]),  
 sum(MenSS$c[MenSS$t==1]==0, na.rm = TRUE) / length(MenSS$e[MenSS$t==1]))
@@ -23,7 +23,7 @@ sum(MenSS$c[MenSS$t==1]==0, na.rm = TRUE) / length(MenSS$e[MenSS$t==1]))
 c(sum(MenSS$e[MenSS$t==2]==1, na.rm = TRUE) / length(MenSS$e[MenSS$t==2]),  
 sum(MenSS$c[MenSS$t==2]==0, na.rm = TRUE) / length(MenSS$e[MenSS$t==2]))
 
-## ----mv, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----mv, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE---------------------------------
 #proportions of missing values in the control group
 c(sum(is.na(MenSS$e[MenSS$t==1])) / length(MenSS$e[MenSS$t==1]),  
 sum(is.na(MenSS$c[MenSS$t==1])) / length(MenSS$e[MenSS$t==1]))
@@ -32,25 +32,25 @@ sum(is.na(MenSS$c[MenSS$t==1])) / length(MenSS$e[MenSS$t==1]))
 c(sum(is.na(MenSS$e[MenSS$t==2])) / length(MenSS$e[MenSS$t==2]),  
 sum(is.na(MenSS$c[MenSS$t==2])) / length(MenSS$e[MenSS$t==2]))
 
-## ----selection1_no, eval=TRUE, echo=FALSE, include=FALSE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----selection1_no, eval=TRUE, echo=FALSE, include=FALSE, comment=NA,warning=FALSE,error=FALSE,message=FALSE------
 NN.sel=selection(data = MenSS, model.eff = e ~ u.0, model.cost = c ~ e, 
   model.me = me ~ age + ethnicity + employment, 
   model.mc = mc ~ age + ethnicity + employment, type = "MAR", 
   n.iter = 1000, dist_e = "norm", dist_c = "norm", ppc = TRUE)
 
-## ----selection1, eval=FALSE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
-#  NN.sel=selection(data = MenSS, model.eff = e ~ u.0, model.cost = c ~ e,
-#    model.me = me ~ age + ethnicity + employment,
-#    model.mc = mc ~ age + ethnicity + employment, type = "MAR",
-#    n.iter = 1000, dist_e = "norm", dist_c = "norm", ppc = TRUE)
+## ----selection1, eval=FALSE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE------------------------
+# NN.sel=selection(data = MenSS, model.eff = e ~ u.0, model.cost = c ~ e,
+#   model.me = me ~ age + ethnicity + employment,
+#   model.mc = mc ~ age + ethnicity + employment, type = "MAR",
+#   n.iter = 1000, dist_e = "norm", dist_c = "norm", ppc = TRUE)
 
-## ----print_selection1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----print_selection1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE-------------------
 print(NN.sel, value.mis = FALSE, only.means = TRUE)
 
-## ----coef_selection1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----coef_selection1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE--------------------
 coef(NN.sel, random = FALSE)
 
-## ----summary_selection1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----summary_selection1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE-----------------
 summary(NN.sel)
 
 ## ----BCEA_selection1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE, fig.width=15,fig.height=9,out.width='65%',fig.align='center'----
@@ -58,36 +58,36 @@ par(mfrow=c(1,2))
 BCEA::ceplane.plot(NN.sel$cea)
 BCEA::ceac.plot(NN.sel$cea)
 
-## ----pattern1_no, eval=TRUE, echo=FALSE, include=FALSE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----pattern1_no, eval=TRUE, echo=FALSE, include=FALSE, comment=NA,warning=FALSE,error=FALSE,message=FALSE--------
 NN.pat=pattern(data = MenSS, model.eff = e ~ u.0, model.cost = c ~ e, 
   type = "MAR", restriction = "CC", n.iter = 1000, Delta_e = 0, Delta_c = 0, 
   dist_e = "norm", dist_c = "norm", ppc = TRUE)
 
-## ----pattern1, eval=FALSE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
-#  NN.pat=pattern(data = MenSS, model.eff = e ~ u.0, model.cost = c ~ e,
-#    type = "MAR", restriction = "CC", n.iter = 1000, Delta_e = 0, Delta_c = 0,
-#    dist_e = "norm", dist_c = "norm", ppc = TRUE)
+## ----pattern1, eval=FALSE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE--------------------------
+# NN.pat=pattern(data = MenSS, model.eff = e ~ u.0, model.cost = c ~ e,
+#   type = "MAR", restriction = "CC", n.iter = 1000, Delta_e = 0, Delta_c = 0,
+#   dist_e = "norm", dist_c = "norm", ppc = TRUE)
 
-## ----coef_pattern1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----coef_pattern1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----------------------
 coef(NN.pat, random = FALSE)
 
-## ----summary_pattern1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----summary_pattern1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE-------------------
 summary(NN.pat)
 
-## ----hurdle1_no, eval=TRUE, echo=FALSE, include=FALSE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----hurdle1_no, eval=TRUE, echo=FALSE, include=FALSE, comment=NA,warning=FALSE,error=FALSE,message=FALSE---------
 NN.hur=hurdle(data = MenSS, model.eff = e ~ u.0, model.cost = c ~ e,
   model.se = se ~ 1, model.sc = sc ~ age, type = "SAR", se = 1, sc = 0,
   n.iter = 1000, dist_e = "norm", dist_c = "norm", ppc = TRUE)
 
-## ----hurdle1, eval=FALSE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
-#  NN.hur=hurdle(data = MenSS, model.eff = e ~ u.0, model.cost = c ~ e,
-#    model.se = se ~ 1, model.sc = sc ~ age, type = "SAR", se = 1, sc = 0,
-#    n.iter = 1000, dist_e = "norm", dist_c = "norm", ppc = TRUE)
+## ----hurdle1, eval=FALSE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE---------------------------
+# NN.hur=hurdle(data = MenSS, model.eff = e ~ u.0, model.cost = c ~ e,
+#   model.se = se ~ 1, model.sc = sc ~ age, type = "SAR", se = 1, sc = 0,
+#   n.iter = 1000, dist_e = "norm", dist_c = "norm", ppc = TRUE)
 
-## ----coef_hurdle1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----coef_hurdle1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE-----------------------
 coef(NN.hur, random = FALSE)
 
-## ----summary_hurdle1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----summary_hurdle1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE--------------------
 summary(NN.hur)
 
 ## ----diag_sel1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE, fig.width=15,fig.height=9,out.width='65%',fig.align='center'----
--- inst/doc/Model_customisation_in_missingHE.R
+++ inst/doc/Model_customisation_in_missingHE.R
@@ -1,10 +1,10 @@
-## ---- echo = FALSE, message = FALSE-------------------------------------------
+## ----echo = FALSE, message = FALSE--------------------------------------------------------------------------------
 knitr::opts_chunk$set(prompt = TRUE, highlight = F, background = '#FFFFFF',
                       collapse = T, comment = "#>")
 library(missingHE)
 set.seed(1234)
 
-## ----menss_data2, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----menss_data2, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE------------------------
 MenSS2 <- MenSS
 MenSS2$e <- MenSS$sex_inst
 
@@ -16,51 +16,51 @@ par(mfrow=c(1,2))
 hist(MenSS2$e[MenSS2$t==1], main = "N sex instances - Control")
 hist(MenSS2$e[MenSS2$t==2], main = "N sex instances - Intervention")
 
-## ----mv, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----mv, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE---------------------------------
 #proportions of missing values in the control group
 sum(is.na(MenSS2$e[MenSS$t==1])) / length(MenSS2$e[MenSS$t==1])  
 
 #proportions of missing values in the intervention group
 sum(is.na(MenSS2$e[MenSS$t==2])) / length(MenSS2$e[MenSS$t==2])  
 
-## ----costs_const, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----costs_const, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE------------------------
  MenSS2$c <- MenSS2$c + 0.01
 
-## ----selection1_no, eval=TRUE, echo=FALSE, include=FALSE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----selection1_no, eval=TRUE, echo=FALSE, include=FALSE, comment=NA,warning=FALSE,error=FALSE,message=FALSE------
 PG.sel=selection(data = MenSS2, model.eff = e ~ sex_inst.0, model.cost = c ~ 1, 
   model.me = me ~ age + ethnicity + employment, 
   model.mc = mc ~ age + ethnicity + employment, type = "MAR", 
   n.iter = 1000, dist_e = "pois", dist_c = "gamma")
 
-## ----selection1, eval=FALSE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
-#  PG.sel=selection(data = MenSS2, model.eff = e ~ sex_inst.0, model.cost = c ~ 1,
-#    model.me = me ~ age + ethnicity + employment,
-#    model.mc = mc ~ age + ethnicity + employment, type = "MAR",
-#    n.iter = 1000, dist_e = "pois", dist_c = "gamma")
+## ----selection1, eval=FALSE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE------------------------
+# PG.sel=selection(data = MenSS2, model.eff = e ~ sex_inst.0, model.cost = c ~ 1,
+#   model.me = me ~ age + ethnicity + employment,
+#   model.mc = mc ~ age + ethnicity + employment, type = "MAR",
+#   n.iter = 1000, dist_e = "pois", dist_c = "gamma")
 
 ## ----plot_selection1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE, fig.width=15,fig.height=9,out.width='65%',fig.align='center'----
 plot(PG.sel, outcome = "effects")
 
-## ----coef_selection1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----coef_selection1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE--------------------
 coef(PG.sel, prob = c(0.05, 0.95))
 
-## ----summary_selection1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----summary_selection1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE-----------------
 summary(PG.sel)
 
-## ----pattern1_no, eval=TRUE, echo=FALSE, include=FALSE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----pattern1_no, eval=TRUE, echo=FALSE, include=FALSE, comment=NA,warning=FALSE,error=FALSE,message=FALSE--------
 PG.pat=pattern(data = MenSS2, model.eff = e ~ sex_inst.0 + (1 | site), 
                model.cost = c ~ 1 + (1 | site), type = "MAR", restriction = "AC", 
                n.iter = 1000, Delta_e = 0, Delta_c = 0, dist_e = "pois", dist_c = "gamma")
 
-## ----pattern1, eval=FALSE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
-#  PG.pat=pattern(data = MenSS2, model.eff = e ~ sex_inst.0 + (1 | site),
-#                 model.cost = c ~ 1 + (1 | site), type = "MAR", restriction = "AC",
-#                 n.iter = 1000, Delta_e = 0, Delta_c = 0, dist_e = "pois", dist_c = "gamma")
+## ----pattern1, eval=FALSE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE--------------------------
+# PG.pat=pattern(data = MenSS2, model.eff = e ~ sex_inst.0 + (1 | site),
+#                model.cost = c ~ 1 + (1 | site), type = "MAR", restriction = "AC",
+#                n.iter = 1000, Delta_e = 0, Delta_c = 0, dist_e = "pois", dist_c = "gamma")
 
-## ----coef_pattern1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----coef_pattern1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----------------------
 coef(PG.pat, random = TRUE)
 
-## ----prior_hurdle1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----prior_hurdle1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----------------------
 my.prior <- list(
   "alpha0.prior" = c(0 , 0.0000001),
   "alpha.prior" = c(0, 0.0000001),
@@ -74,7 +74,7 @@ my.prior <- list(
   "sigma.prior.c" = c(0, 10000)
 )
 
-## ----hurdle1_no, eval=TRUE, echo=FALSE, include=FALSE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----hurdle1_no, eval=TRUE, echo=FALSE, include=FALSE, comment=NA,warning=FALSE,error=FALSE,message=FALSE---------
 #remove added constant from costs
 #MenSS2$c <- MenSS2$c - 0.01
 
@@ -82,17 +82,17 @@ PG.hur=hurdle(data = MenSS2, model.eff = e ~ sex_inst.0, model.cost = c ~ 1 + (1
   model.se = se ~ 1, model.sc = sc ~ age + (1 | site), type = "SAR", se = 1, sc = 0,
   n.iter = 1000, dist_e = "pois", dist_c = "gamma", prior = my.prior)
 
-## ----hurdle1, eval=FALSE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
-#  #remove added constant from costs
-#  #MenSS2$c <- MenSS2$c - 0.01
-#  
-#  PG.hur=hurdle(data = MenSS2, model.eff = e ~ sex_inst.0, model.cost = c ~ 1 + (1 | site),
-#    model.se = se ~ 1, model.sc = sc ~ age + (1 | site), type = "SAR", se = NULL, sc = 0,
-#    n.iter = 1000, dist_e = "pois", dist_c = "gamma", prior = my.prior)
+## ----hurdle1, eval=FALSE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE---------------------------
+# #remove added constant from costs
+# #MenSS2$c <- MenSS2$c - 0.01
+# 
+# PG.hur=hurdle(data = MenSS2, model.eff = e ~ sex_inst.0, model.cost = c ~ 1 + (1 | site),
+#   model.se = se ~ 1, model.sc = sc ~ age + (1 | site), type = "SAR", se = NULL, sc = 0,
+#   n.iter = 1000, dist_e = "pois", dist_c = "gamma", prior = my.prior)
 
-## ----coef_hurdle1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----coef_hurdle1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE-----------------------
 coef(PG.hur, random = FALSE)
 
-## ----summary_hurdle1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE----
+## ----summary_hurdle1, eval=TRUE, echo=TRUE, comment=NA,warning=FALSE,error=FALSE,message=FALSE--------------------
 coef(PG.hur, random = TRUE)
 
